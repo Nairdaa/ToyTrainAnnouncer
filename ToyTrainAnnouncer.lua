c1823855e54a7403b274bf9431badb861a92ed7f
Nairda <38843773+Nairdaa@users.noreply.github.com>
2024-06-03 05:09:28 +0200

    Update ToyTrainAnnouncer.lua
    Deshitify code even more and put "documentation". Big project is big

--- ToyTrainAnnouncer.lua
+++ ToyTrainAnnouncer.lua
@@ -1,16 +1,19 @@
+-- Create a frame for the addon
 local ToyTrainDetector = CreateFrame("Frame")
-local ToyTrainSpellID = 61031
 
---ToyTrainDetector:RegisterEvent("UNIT_SPELLCAST_START")
+-- Define the Toy Train Set spell ID - Don't wanna hardcode it... at least yet.
+local TOY_TRAIN_SPELL_ID = 61031
+
+-- Register the UNIT_SPELLCAST_SUCCEEDED event to detect when the spell is successfully cast
 ToyTrainDetector:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 
+-- Event handler function
 ToyTrainDetector:SetScript("OnEvent", function(self, event, unitID, _, spellID)
-	if spellID == ToyTrainSpellID then
-		local playerName = UnitName(unitID)
-		--if event == "UNIT_SPELLCAST_START" then
-			--SendChatMessage("Detected Toy Train Set cast by: " .. playerName, "PARTY")
-		if event == "UNIT_SPELLCAST_SUCCEEDED" then
-			SendChatMessage("Toy Train Set placed by: " .. playerName, "PARTY")
-		end
-	end
+    if spellID == TOY_TRAIN_SPELL_ID then
+        local playerName = UnitName(unitID)
+        if playerName then
+            -- Send a chat message to the party channel
+            SendChatMessage("Toy Train Set placed by: " .. playerName, "PARTY")
+        end
+    end
 end)
