a9af0eadff9428ae647c379c765b21587aafebe1
Nairda <38843773+Nairdaa@users.noreply.github.com>
2024-06-03 06:47:07 +0200

    Update ToyTrainAnnouncer.lua
    yesn't

--- ToyTrainAnnouncer.lua
+++ ToyTrainAnnouncer.lua
@@ -4,6 +4,10 @@ local ToyTrainDetector = CreateFrame("Frame")
 -- Define the Toy Train Set spell ID - Don't wanna hardcode it... at least yet.
 -- Wondering if there is a way to auto-find the offsets for spell/item IDs and update them "on-the-go" when the plugin is loading. 
 -- It's possible with the Source Engine, but Blizz' API sucks even more imo. Soon™.
+--
+-- Note to self: It should be able to, I think. Depends what Blizzard lets you do with the files inside of the /plugin folder.
+-- Possible breach detected xd
+
 local TOY_TRAIN_SPELL_ID = 61031
 
 -- Register the UNIT_SPELLCAST_SUCCEEDED event to detect when the spell is successfully cast
