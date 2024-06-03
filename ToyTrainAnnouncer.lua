66c11afa30896570944abf837c1c38f0ffb10696
Nairda <38843773+Nairdaa@users.noreply.github.com>
2024-06-03 05:01:41 +0200

    Update ToyTrainAnnouncer.lua
    change cast detection to a comment because useless

--- ToyTrainAnnouncer.lua
+++ ToyTrainAnnouncer.lua
@@ -5,12 +5,12 @@ ToyTrainDetector:RegisterEvent("UNIT_SPELLCAST_START")
 ToyTrainDetector:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 
 ToyTrainDetector:SetScript("OnEvent", function(self, event, unitID, _, spellID)
-    if spellID == toyTrainSpellID then
-        local playerName = UnitName(unitID)
-        if event == "UNIT_SPELLCAST_START" then
-            SendChatMessage("Detected Toy Train Set cast by: " .. playerName, "PARTY")
-        elseif event == "UNIT_SPELLCAST_SUCCEEDED" then
-            SendChatMessage("Toy Train Set placed by: " .. playerName, "PARTY")
-        end
-    end
+	if spellID == toyTrainSpellID then
+		local playerName = UnitName(unitID)
+		--if event == "UNIT_SPELLCAST_START" then
+			--SendChatMessage("Detected Toy Train Set cast by: " .. playerName, "PARTY")
+		if event == "UNIT_SPELLCAST_SUCCEEDED" then
+			SendChatMessage("Toy Train Set placed by: " .. playerName, "PARTY")
+		end
+	end
 end)
