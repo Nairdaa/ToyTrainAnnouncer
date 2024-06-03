8903ddc3aaa135731f9d4b4793db2d181083f692
Nairda <38843773+Nairdaa@users.noreply.github.com>
2024-06-03 03:49:45 +0200

    Update ToyTrainAnnouncer.lua
    deshitified the code

--- ToyTrainAnnouncer.lua
+++ ToyTrainAnnouncer.lua
@@ -1,26 +1,16 @@
-ToyTrainDetector = CreateFrame("Frame")
-ToyTrainDetectorSuccess = CreateFrame("Frame")
-
+local ToyTrainDetector = CreateFrame("Frame")
 local toyTrainSpellID = 61031
 
 ToyTrainDetector:RegisterEvent("UNIT_SPELLCAST_START")
-ToyTrainDetectorSuccess:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+ToyTrainDetector:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 
-ToyTrainDetector:SetScript("OnEvent", function(self, event, ...)
-	local unitID, _, spellID = ...
-	if spellID == toyTrainSpellID then
-		local playerName = UnitName(unitID)
-
-		--print("Debug: " .. playerName .. " started casting Toy Train Set (Spell ID: " .. spellID .. ")")
-
-		SendChatMessage("Detected Toy Train Set cast by: " .. playerName, "PARTY")
-	end
-end)
-
-ToyTrainDetectorSuccess:SetScript("OnEvent", function(self, event, ...)
-	local unitID, _, spellID = ...
-	if spellID == toyTrainSpellID then
-		local playerName = UnitName(unitID)
-		SendChatMessage("Toy Train Set placed by: " .. playerName, "PARTY")
-	end
+ToyTrainDetector:SetScript("OnEvent", function(self, event, unitID, _, spellID)
+    if spellID == toyTrainSpellID then
+        local playerName = UnitName(unitID)
+        if event == "UNIT_SPELLCAST_START" then
+            SendChatMessage("Detected Toy Train Set cast by: " .. playerName, "PARTY")
+        elseif event == "UNIT_SPELLCAST_SUCCEEDED" then
+            SendChatMessage("Toy Train Set placed by: " .. playerName, "PARTY")
+        end
+    end
 end)
