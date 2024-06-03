4e9d5a13c9693ace1ccf7ab013b757657da7404d
Nairda <38843773+Nairdaa@users.noreply.github.com>
2024-06-03 05:03:25 +0200

    Update ToyTrainAnnouncer.lua
    deshitify code even more

--- ToyTrainAnnouncer.lua
+++ ToyTrainAnnouncer.lua
@@ -1,11 +1,11 @@
 local ToyTrainDetector = CreateFrame("Frame")
-local toyTrainSpellID = 61031
+local ToyTrainSpellID = 61031
 
-ToyTrainDetector:RegisterEvent("UNIT_SPELLCAST_START")
+--ToyTrainDetector:RegisterEvent("UNIT_SPELLCAST_START")
 ToyTrainDetector:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 
 ToyTrainDetector:SetScript("OnEvent", function(self, event, unitID, _, spellID)
-	if spellID == toyTrainSpellID then
+	if spellID == ToyTrainSpellID then
 		local playerName = UnitName(unitID)
 		--if event == "UNIT_SPELLCAST_START" then
 			--SendChatMessage("Detected Toy Train Set cast by: " .. playerName, "PARTY")
